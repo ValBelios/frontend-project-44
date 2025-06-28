8e1d413e7f426f867d08866b329d942a45b5389c
Valery <wer1wer1wer1@mail.ru>
2025-06-28 17:52:31 +0500

    fix: исправления по линтингу для CI

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-const getRandomIndex = (data) => Math.floor(Math.random() * data.length)
+const getRandomIndex = data => Math.floor(Math.random() * data.length)
 
 export { getRandomNumber, getRandomIndex }
