304a0ba1c5e12c31feb70093ba4967c87df925b4
Valery <wer1wer1wer1@mail.ru>
2025-06-28 17:08:49 +0500

    fix: убраны лишние точки с запятой и добавлены переводы строк

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,31 +1,27 @@
 #!/usr/bin/env node
-import { getRandomNumber } from '../utils.js';
-import run from '../index.js';
-
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const minRange = 0;
-const maxRange = 100;
-
+import { getRandomNumber } from '../utils.js'
+import run from '../index.js'
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+const minRange = 0
+const maxRange = 100
 const isPrime = (number) => {
-  const limit = Math.sqrt(number);
+  const limit = Math.sqrt(number)
   if (number <= 1) {
-    return false;
+    return false
   }
   for (let i = 2; i <= limit; i += 1) {
     if (number % i === 0) {
-      return false;
+      return false
     }
   }
-  return true;
-};
-
+  return true
+}
 const generateRound = () => {
-  const number = getRandomNumber(minRange, maxRange);
-  const question = number.toString();
-  const correctAnswer = isPrime(number) ? 'yes' : 'no';
-  return [question, correctAnswer];
-};
-
+  const number = getRandomNumber(minRange, maxRange)
+  const question = number.toString()
+  const correctAnswer = isPrime(number) ? 'yes' : 'no'
+  return [question, correctAnswer]
+}
 export default () => {
-  run(description, generateRound);
-};
+  run(description, generateRound)
+}
